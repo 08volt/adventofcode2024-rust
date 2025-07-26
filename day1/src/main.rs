09172e4d049b1e8053833bb67cd62afe3baf1ea6
enricovoltan <voltan.enrico@gmail.com>
2025-07-26 14:55:08 +0200

    removed target

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,44 +1,60 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::iter::{zip, Map};
 use std::path::Path;
+use std::result;
 
 fn main() {
 
     const FILE_PATH: &str = "./input.txt";
 
-    let _numbers: [&str; 9] = [ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
-
-    let mut result = 0;
+    let mut first_list: Vec<i32> = Vec::new();
+    let mut second_list: Vec<i32> = Vec::new();
 
     if let Ok(lines) = read_lines(FILE_PATH) {
-        // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(line) = line {
 
-                let a = first_digit(&line, _numbers);          
+                let mut numbers = line.split_whitespace();
 
-                let reversed_line: String = line.chars().rev().collect();  
-                let reversed_numbers = _numbers.map(|n: &str| {
-                    n.chars().rev().collect::<String>()
-                });
+                let first_number = numbers.next().unwrap().parse::<i32>().unwrap();
+                let second_number = numbers.next().unwrap().parse::<i32>().unwrap();
 
-                let b = first_digit(reversed_line, reversed_numbers);   
-                
-                match (a,b) {
-                    (Some(a), Some(b)) => {
-                        println!("{}{}", a, b);
-                        let n = format!("{}{}", a, b).parse::<i32>().unwrap();
-                        result = result + n;
-                    },
-                    (_,_) => ()
-                }
-                
+                first_list.push(first_number.clone());
+                second_list.push(second_number.clone());
                 
             }
         }
     }
+
+    first_list.sort();
+    second_list.sort();
+
+    let mut result = 0;
+
+    zip(first_list.clone(), second_list.clone()).for_each(|(a, b)| {
+        result += (a - b).abs( ) as i32;
+    });
+
     println!("{}", result);
+
+    let mut second_list_count: HashMap<i32, usize> = HashMap::new();
+
+    for number in second_list.iter() {
+        *second_list_count.entry(number.clone()).or_insert(0) += 1;
+    }
+
+
+    let result2 = first_list
+        .iter()
+        .map(| value| value * *second_list_count.get(value).unwrap_or(&0) as i32 )
+        .sum::<i32>();
+
+    println!("{:#?}", second_list_count);
+
+    println!("{}", result2);
 }
 
 // The output is wrapped in a Result to allow matching on errors
@@ -47,27 +63,4 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
-}
-
-fn first_digit(line: impl Into<String>, numbers: [impl Into<String>; 9]) -> Option<char>{
-    let line: String = line.into();
-
-    let mut smallest_index = line.chars().position(|c| c.is_digit(10)).unwrap_or_else(|| line.len());
-    let mut smallest_res = line.chars().find(|c| c.is_digit(10));
-
-    for (pos, number) in numbers.into_iter().enumerate() {        
-        let i = line.find(&number.into());
-        match i {
-            Some(index) => {
-                if index < smallest_index {
-                    smallest_index = index;
-                    smallest_res = std::char::from_digit((pos + 1).try_into().unwrap_or_default(), 10);
-                }
-            },
-            None => (),
-        }
-    }
-    
-    smallest_res
-
 }
