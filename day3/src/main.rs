c04a1fe3389c86cbec8658215749dbf962ff5a6f
enricovoltan <voltan.enrico@gmail.com>
2025-07-26 23:15:59 +0200

    day 3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,59 +1,43 @@
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::iter::{zip, Map};
+use regex::Regex;
 use std::path::Path;
-use std::result;
-use std::str::FromStr;
-
-
-struct Report {
-    levels: Vec<i32>
-}
-
-impl FromStr for Report {
-    type Err = io::Error;
-
-    fn from_str(s: &str) -> result::Result<Self, Self::Err> {
-        let levels: Vec<i32> = s.split_whitespace()
-            .map(|n| n.parse().unwrap())
-            .collect();
-        Ok(Report { levels })
-    }
-}
-
-impl Report {
-    fn is_safe(&self, threshold_min: i32, threshold_max: i32) -> bool {
-        self.levels.windows(2).all(|pair| threshold_min <= (pair[1] - pair[0]) && (pair[1] - pair[0]) <= threshold_max)
-        || self.levels.windows(2).all(|pair| threshold_min <= (pair[0] - pair[1]) && (pair[0] - pair[1]) <= threshold_max)
-        || self.levels.len() == 1
-    }
-}
-
-
 
 
 fn main() {
 
     const FILE_PATH: &str = "./input.txt";
 
+    
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\)").unwrap();
 
-    // let mut input_matrix: Vec<Report> = Vec::new();
-
-    let mut safe_reports = 0;
-
+    let mut enabled = true;
+    // Open the text file
     if let Ok(lines) = read_lines(FILE_PATH) {
-        for line in lines {
+        let final_result: u32 = lines.map(|line| {
+            let mut result = 0;
             if let Ok(line) = line {
-
-                let report = Report::from_str(&line).unwrap();
-                if report.is_safe(1, 3) {
-                    safe_reports += 1;
+                
+                for cap in re.captures_iter(&line) {
+                    if let Some(mul_match) = cap.get(0) {
+                        if mul_match.as_str().starts_with("mul") && enabled{
+                            let x: u32 = cap[1].parse().unwrap();
+                            let y: u32 = cap[2].parse().unwrap();
+                            result += x * y;
+                        } else if mul_match.as_str() == "don't()" {
+                            enabled = false;
+                            println!("Found: don't()");
+                        } else if mul_match.as_str() == "do()" {
+                            enabled = true;
+                            println!("Found: do()");
+                        }
+                    }
                 }
             }
-        }
+            return result
+        }).sum();
+        println!("The final result is: {}", final_result);
     }
-    println!("Number of safe reports: {}", safe_reports);
 }
 
 // The output is wrapped in a Result to allow matching on errors
